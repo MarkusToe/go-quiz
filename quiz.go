9e422339fd39f4a2fba0a93238ad5c6becc73558
Markus Török <markus.toeroek@wikitude.com>
2019-11-13 09:11:25 +0100

    Read CSV file

--- quiz.go
+++ quiz.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
+	csvFile, _ := os.Open("problems.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			break
+		}
+
+		fmt.Println(line)
+	}
+
 	fmt.Println("Hello World!")
 }
